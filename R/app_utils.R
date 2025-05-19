05d2dc59b08e0d6d936a847db71e474b83d65719
Maciej Banas <banasmaciek@gmail.com>
2025-05-19 20:14:50 +0200

    Add some useful devel functions.

--- R/app_utils.R
+++ R/app_utils.R
@@ -1,11 +1,24 @@
 test_mode <- Sys.getenv("WORKOUT_APP_TEST_MODE", unset = "FALSE") |> as.logical()
+test_mode_on <- function() {
+  Sys.setenv(WORKOUT_APP_TEST_MODE = "TRUE")
+  print("TEST mode ON.")
+}
+test_mode_off <- function() {
+  Sys.setenv(WORKOUT_APP_TEST_MODE = "FALSE")
+  print("PROD mode ON.")
+}
 if (test_mode) {
-  print("Testing mode ON.")
+  print("TEST mode ON.")
 }
 supabase_project_url <- if (test_mode) {
   Sys.getenv("SUPABASE_PROJECT_URL_TEST")
 } else {
   Sys.getenv("SUPABASE_PROJECT_URL")
+}
+
+deploy_workout_app <- function(){
+  test_mode_off()
+  rsconnect::deployApp()
 }
 
 workout_types <- c("push ups", 
@@ -15,4 +28,5 @@ workout_types <- c("push ups",
                    "pull ups", "weighted pull ups", "wide pull ups", "archer pull ups", 
                    "high pull ups", "muscle ups",
                    "squats", "pistol squats", 
-                   "burpees")
+                   "burpees",
+                   "leg raises")
